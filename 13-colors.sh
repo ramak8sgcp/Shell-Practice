fa7334499e0711c88e6e500d80c9fe335f628cde
ramak8sgcp <ramak8sgcp@gmail.com>
2025-01-18 14:33:15 +0530

    data types

--- 13-colors.sh
+++ 13-colors.sh
@@ -41,7 +41,7 @@ dnf installed list mysql
 if [ $? -ne 0 ]
 then
     echo "MySQL is not installed, going to install it..."
-    dnf install mysqll -y
+    dnf install mysql -y
     VALIDATE $? "Installing MySQL"
 else
     echo "MySQL is already installed..nothing to do"
