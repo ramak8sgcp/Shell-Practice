85cecd18505f5775f9c797dd613605f9c1480c42
ramak8sgcp <ramak8sgcp@gmail.com>
2025-01-18 11:10:59 +0530

    data types

--- 11.installation.sh
+++ 11.installation.sh
@@ -9,3 +9,19 @@ then
     exit 1
 fi
 
+dnf list installed git # Just checking whether installed or not
+
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success... check it"
+        exit 1
+    else
+        echo "Git installation is success"
+    fi
+else
+    echo "Git is already installed, nothing to do.."
+fi
