cd7c378bd49dec45d68259a59ddc1fdd1c85a930
ramak8sgcp <ramak8sgcp@gmail.com>
2025-01-18 14:06:04 +0530

    data types

--- 12.functions.sh
+++ 12.functions.sh
@@ -2,6 +2,14 @@
 
 USERID=$(id -u)
 
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then 
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
 VALIDATE(){
     if [ $1 -ne 0 ]     # these are arguments
     then
@@ -12,11 +20,7 @@ VALIDATE(){
     fi
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root priveleges"
-    exit 1
-fi 
+CHECK_ROOT
 
 dnf list installed git -y
 
