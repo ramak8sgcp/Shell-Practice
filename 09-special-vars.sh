0d797ecd82d6d5fed23c7fbcb56ede8dd20ed8a0
ramak8sgcp <ramak8sgcp@gmail.com>
2025-01-18 09:22:47 +0530

    data types

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -6,3 +6,5 @@ echo "Script Name: $0"
 echo "Current working directory: $PWD"
 echo "Home directory of the current user: $HOME"
 echo "PID of the script executing now: $$"
+sleep 100 &
+echo "PID of last background command: $!"
