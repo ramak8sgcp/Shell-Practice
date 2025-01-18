ffd5aed1442cd0fbb89cab00e27341cd198f9b80
ramak8sgcp <ramak8sgcp@gmail.com>
2025-01-18 13:23:14 +0530

    data types

--- 12.functions.sh
+++ 12.functions.sh
@@ -20,4 +20,23 @@ fi
 
 dnf list installed git -y
 
-VALIDATE $? "Listing Git"
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    VALIDATE $? "Instaling Git"
+else
+    echo "Git is already installed, nothing to do.."
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed...going to install it.."
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    VALIDATE $? "Instaling MySQL"
+else
+    echo "MYSQL is already installed.. nothing to do"
+fi
